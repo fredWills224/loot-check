1a39abbd3f29a11d101f9061063469ee8aa8a599
fredWills224 <fredwills4229@gmail.com>
2020-10-07 00:11:12 -0400

    mockWithdraw

--- src/components/Wallet.jsx
+++ src/components/Wallet.jsx
@@ -1,6 +1,6 @@
 import React, {  Component } from 'react';
 import { connect } from 'react-redux';
-import { deposit } from '../actions/balance';
+import { deposit, withdraw } from '../actions/balance';
 
 export class Wallet extends Component {
     constructor(){
@@ -9,8 +9,8 @@ export class Wallet extends Component {
     }
 
     updateBalance = event => this.setState({ balance: parseInt(event.target.value, 10) });
-
     deposit = () => this.props.deposit(this.state.balance);
+    withdraw = () => this.props.withdraw(this.state.balance);
 
     render(){
         return(
@@ -20,10 +20,11 @@ export class Wallet extends Component {
                 <br/>
                 <input className='input-wallet' onChange={this.updateBalance} />
                 <button className='btn-deposit' onClick={this.deposit} >Deposit</button>
+                <button className='btn-withdraw' onClick={this.withdraw}>Withdraw</button>
 
             </div>
         )
     }
 }
 
-export default connect( state => {return { balance: state }}, { deposit } )(Wallet);
+export default connect( state => {return { balance: state }}, { deposit, withdraw } )(Wallet);
