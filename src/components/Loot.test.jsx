3b3f6fccb7510533d8d450c6e2cefb9b5bd6f78c
fredWills224 <fredwills4229@gmail.com>
2020-10-08 02:41:10 -0400

    loot component complete

--- src/components/Loot.test.jsx
+++ src/components/Loot.test.jsx
@@ -5,7 +5,7 @@ import { Loot } from './Loot';
 
 describe('Loot', ()=>{
     const mockFetchbitcoin = jest.fn();
-    const props = { balance: 10, bitcoin: {}, fetchBitcoin: mockFetchbitcoin}
+    let props = { balance: 10, bitcoin: {}, fetchBitcoin: mockFetchbitcoin}
     let loot = shallow(<Loot {...props} />);
 
     it('renders properly', ()=>{
@@ -21,6 +21,19 @@ describe('Loot', ()=>{
 
         it('dispatches the `fetchBitcoin()` method it receives from props', () =>{
             expect(mockFetchbitcoin).toHaveBeenCalled();
+        });
+
+    });
+
+    describe('when there are valid bitcoin props', ()=>{
+        
+        beforeEach(() =>{
+            props = {balance: 10, bitcoin: { bpi: { USD: { rate:'1,000' } } }, fetchBitcoin: mockFetchbitcoin };
+            loot= shallow(<Loot {...props} />)
+        });
+
+        it('displays the correct bitcoin value', ()=>{
+            expect(loot.find('h3').text()).toEqual('Bitcoin balance: 0.01');
         });
 
     });
